9d76238d7b6d4070007dd281168ffbd40398d691
daaviidmartinez <44229292+daaviidmartinez@users.noreply.github.com>
2019-01-29 12:18:55 +0100

    Add files via upload
    actividad refuerzo 3

---
+++ actividad_refuerzo3.php
@@ -0,0 +1,35 @@
+<?php
+$conexion = new mysqli("localhost", "root", "", "liga");
+if ($conexion->connect_errno) {
+    echo "Fallo al conectar a MySQL: (" . $conexion->connect_errno . ") " . $conexion->connect_error;
+}else {
+    $resultado = $conexion->query("SELECT id_equipo,nombre,ciudad FROM equipo");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<table>
+    <tr>
+        <td>ID</td>
+        <td>Nombre</td>
+        <td>Top 5 alturas</td>
+    </tr>
+    <?php
+     foreach ($resultado as $equipo) {
+         echo "<tr>";
+         echo "<td>".$equipo['id_equipo']."</td>";
+         echo "<td>".$equipo['nombre']."</td>";
+         echo "<td>".$equipo['ciudad']."</td>";
+         echo "</tr>";
+     }
+    ?>
+ </table>
+</body>
+</html>
